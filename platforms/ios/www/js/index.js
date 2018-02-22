ab913074255fbe6ba8756d3b1a6142e2f543eb76
rwbarkeriv <rwbarkeriv@gmail.com>
2018-02-22 13:55:59 +0200

    Phase 2.1: Refactored code and added simple styling.

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function () {
@@ -40,13 +22,15 @@ var app = {
     }
 };
 
-var map, marker, infoWindow, geocoder;
+var map, marker, infoWindow;
 
 function initMap() {
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 65.0121, lng: 25.4651},
         zoom: 6
     });
+
     var geocoder = new google.maps.Geocoder();
 
     document.getElementById('submit').addEventListener('click', function () {
@@ -57,9 +41,9 @@ function initMap() {
         calculateDistance(geocoder, map);
     });
 
-    document.getElementById('reverse-geocode').addEventListener('click', function () {
-        geocodeLatLng(geocoder, map);
-    });
+    // document.getElementById('reverse-geocode').addEventListener('click', function () {
+    //     geocodeLatLng(geocoder, map);
+    // });
 
 
     if (navigator.geolocation) {
@@ -127,41 +111,6 @@ function geocodeAddress(geocoder, resultsMap) {
 
 }
 
-function geocodeLatLng(geocoder, resultsMap) {
-    var input = document.getElementById('address').value;
-    var latlngStr = input.split(',', 2);
-    var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-    geocoder.geocode({'location': latlng}, function (results, status) {
-        if (status === 'OK') {
-            if (results[0]) {
-                resultsMap.setCenter(results[0].geometry.location);
-                resultsMap.setZoom(7);
-                if (marker && marker instanceof google.maps.Marker) {
-                    marker.setMap(null);
-                    marker = null;
-                }
-
-                marker = new google.maps.Marker({
-                    position: latlng,
-                    map: resultsMap
-                });
-                var infowindow = new google.maps.InfoWindow({
-                    content: results[0].formatted_address,
-                });
-
-                google.maps.event.addListener(marker, 'click', function () {
-                    infowindow.open(map, marker);
-                });
-
-            } else {
-                window.alert('No results found');
-            }
-        } else {
-            window.alert('Geocoder failed due to: ' + status);
-        }
-    });
-}
-
 function calculateDistance(geocoder, resultsMap) {
 
     function findPointA(geocoder, resultsMap, callback) {
@@ -176,6 +125,14 @@ function calculateDistance(geocoder, resultsMap) {
                 marker = new google.maps.Marker({
                     map: resultsMap,
                     position: results[0].geometry.location,
+                });
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<p>Coordinates:' + marker.getPosition() + '</p>'
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.open(resultsMap, marker);
                 });
                 callback(results[0].geometry.location.lat(), results[0].geometry.location.lng())
 
@@ -195,6 +152,14 @@ function calculateDistance(geocoder, resultsMap) {
                     map: resultsMap,
                     position: results[0].geometry.location
                 });
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<p>Coordinates:' + marker.getPosition() + '</p>'
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.open(resultsMap, marker);
+                });
                 callback(results[0].geometry.location.lat(), results[0].geometry.location.lng())
 
             } else {
@@ -210,9 +175,7 @@ function calculateDistance(geocoder, resultsMap) {
             document.getElementById('distance-output').innerHTML = "<p>" + (google.maps.geometry.spherical.computeDistanceBetween(pointA, pointB) / 1000).toFixed(2) + " kilometers</p>";
         });
     })
-
 }
-
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
